90ccd0eea4493f82e6b842987914e5793b34da69
Bright Phiri <bphiri.aki@gmail.com>
2023-04-01 17:25:32 +0200

    WIP

--- src/services/LineItemService.js
+++ src/services/LineItemService.js
@@ -4,10 +4,10 @@ export default {
     getData(){
         return Api().get('items')
     },
-    create(employee_id,order_id,line_item) {
+    create(employee_id, order_id, line_item) {
         return Api().post(`employees/${employee_id}/orders/${order_id}/line_items`, line_item)
     },
-    update(employee_id,order_id,line_item, line_item_id) {
+    update(employee_id, order_id,line_item, line_item_id) {
         return Api().put(`employees/${employee_id}/orders/${order_id}/line_items/${line_item_id}`, line_item)
     },
     delete(employee_id, order_id, line_item_id){
